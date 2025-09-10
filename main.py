a829bdfdec16f3cb15c939a1190f815da465f2c6
Vinicius Rodrigues <viniciusrf2@al.insper.edu.br>
2025-09-10 08:48:48 -0300

    Roteiro 4 - Add node classes

--- main.py
+++ main.py
@@ -1,4 +1,45 @@
 import sys
+from abc import ABC, abstractmethod
+
+class Node(ABC):
+    def __init__(self, value: int | str, children):
+        self.value = value
+        self.children = children
+
+    @abstractmethod
+    def evaluate(self):
+        pass
+
+class IntVal(Node):
+    def __init__(self, value: int | str):
+        super().__init__(value, [])
+
+    def evaluate(self):
+        return self.value
+    
+class UnOp(Node):
+    def __init__(self, value: int | str, child: Node):
+        super().__init__(value, [child])
+
+    def evaluate(self):
+        if(self.value == '+'):
+            return self.children[0].evaluate()
+        elif(self.value == '-'):
+            return -self.children[0].evaluate()
+    
+class BinOp(Node):
+    def __init__(self, value: int | str, children: list[Node]):
+        super().__init__(value, children)
+
+    def evaluate(self):
+        if(self.value == '+'):
+            return self.children[0].evaluate() + self.children[1].evaluate()
+        elif(self.value == '-'):
+            return self.children[0].evaluate() - self.children[1].evaluate()
+        elif(self.value == '*'):
+            return self.children[0].evaluate() * self.children[1].evaluate()  
+        elif(self.value == '/'):
+            return self.children[0].evaluate() / self.children[1].evaluate()
 
 class Token:
     def __init__(self, kind: str, value: int | str):
